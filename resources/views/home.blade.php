02b6cd27690fbf74f9739b397f1f5217c567fb00
tomelsom <te28@hotmail.com>
2023-10-15 21:39:17 +0100

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Add these links to include Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1j76PVC5p6tp/q/s5KzSvN" crossorigin="anonymous">
+
+<!-- Add this link to include Bootstrap JavaScript (optional) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-OUk6gg/rOvtr3IH1XwnrAB5ssRzS2FpCzVbFzKlNmnFSY4iExr2k+5U3MI5H8mNd" crossorigin="anonymous"></script>
+
+  <title>Document</title>
+</head>
+<body>
+  <h1>testing 123</h1>
+  <div class="card">
+    <h2>form</h2>
+  </div>
+</body>
+</html>
